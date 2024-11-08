b756c0fc63c1cdcf278cc3b325522e3d281ecf13
Sak-06 <sakshigangwar2003@gmail.com>
2024-11-08 15:28:20 +0530

    assessment1

---
+++ script.js
@@ -0,0 +1,16 @@
+const name=document.getElementsByName('name');
+const email=document.getElementsByName('emailid');
+const phone=document.getElementsByName('phnum');
+const linkid=document.getElementsByName('linkid');
+const gitid=document.getElementsByName('gitid');
+const exp=document.getElementsByName('exp');
+const ed=document.getElementsByName('ed');
+const photo=document.getElementsByName('photo');
+
+
+
+function generateresume(){
+    if(button.clicked){
+    entries.style.visibility='hidden';
+    }
+}
